f0f0fd284b027b09ab9e87e3b1a2d0463e902275
0x4eb11004 <34691959+Dxvistxr@users.noreply.github.com>
2019-08-25 13:22:46 +0200

    Update sh_code.py

--- sh_code.py
+++ sh_code.py
@@ -1,17 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/python2
 import sys
+
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
-		print "usage: %s file.bin\n" % (sys.argv[0],)
+		print("usage : %s <file>\n" % (sys.argv[0],))
 		sys.exit(0)
 
 	shellcode = "\""
 	ctr = 1
-	maxlen = 15
+	maxlenght = 15
 
 	for b in open(sys.argv[1], "rb").read():
 		shellcode += "\\x" + b.encode("hex")
-		if ctr == maxlen:
+		if ctr == maxlenght:
 			shellcode += "\"\n\""
 			ctr = 0
 		ctr += 1
